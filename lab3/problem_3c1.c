b9d199e853cf2ab4b87fa46f02f75b923c3b95be
V-Crypt <js_monterrubio.ec@outlook.com>
2021-07-02 01:49:07 +0000

    Laboratorio 3

---
+++ lab3/problem_3c1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float Resistor, current;
+
+    printf("Enter the current value: ");
+    scanf("%f", &current);
+
+    extern float resistor_fun(float);
+
+    Resistor = resistor_fun( current );
+
+    printf("The required resistor should be ");
+    printf("%1.0f Ohms.\n", Resistor);
+}
---
+++ lab3/prog01.c
@@ -0,0 +1,17 @@
+/* My first code in C
+   Jesus Monterrubio
+*/
+
+/* Instructions starting with pound (#) are preprocessor commands */
+
+#define BIGNUM 1000000
+#include<stdio.h>
+
+int main(void)
+{
+    int a= BIGNUM;
+
+    printf("Hello, world, I am %d happy units\n", a);
+
+return 0;
+}
